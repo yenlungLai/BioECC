e82451aed438e1ce54cf0a25333222634ad0ce74
yenlung <yenlung56@gmail.com>
2021-11-20 13:54:12 +0800

    done field added

--- Run_RECC_finite.m
+++ Run_RECC_finite.m
@@ -2,38 +2,43 @@ clear all
 load('x1.mat')
 load('x2.mat')
 
-n=11;% n>k, n needd large to show correctness
-t=5; % maximum is: floor((k)/2);
-k=10; % 
-countmax=n; % countmax is to the size of polynomial p(n)
+mbit=7; % mbit control the field size F_mbit for the generator matrix
+n=5;% n>k, n needd large to show correctness
+t=2; % maximum is: floor((k)/2);
+k=4; % 
+no_codewords=n^2; % countmax is to the size of polynomial p(n)
 
 
 %generate random key x or weight t
 rx = [ones(1,t), zeros(1,k-t)]; 
-rx=rx( randperm(k) )';
+x=rx( randperm(k) )';
 
 
 
-y=[];count=0; 
-while isempty(y) && count<=countmax
+
+y_prime=[];trial=0; 
+while isempty(y_prime) && trial<=no_codewords
+    
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Encoding %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[binary_M, query_codeword,proj_n_mat]=encode_T(x1',n,k,rx);
+[y,matrix_F]=encode_T(x1',n,k,mbit,x);
+ 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Decoding %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[G,x,y]=decoding(x2',n,k,t,query_codeword,proj_n_mat);
+[G_prime,x_prime,y_prime]=decoding(x2',n,k,t,mbit,y,matrix_F);
 
 
-
-if isempty(y) && count==countmax
+if isempty(y_prime) && trial==no_codewords
     disp('no solution')
+
 end 
-count=count+1;
-count
+trial=trial+1;
+trial
 end
 
 
-En=['Encoded Key  : ' int2str(rx)'];
-Rec=['Recovered Key: ' int2str(x)'];
+En=['Encoded Key  : ' int2str(x)'];
+Rec=['Recovered Key: ' int2str(x_prime)'];
  disp(En)
  disp(Rec)
 
@@ -41,92 +46,87 @@ Rec=['Recovered Key: ' int2str(x)'];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Algorithms %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [G,x,y]=Decode_T(binary_M,k,error_max,query_codeword)
-error_weight=error_max;
+function [G,x,y]=Decode_T(binary_M,k,t,m,query_codeword)
+error_weight=t;
 % d=n-k+1;%define 2^m=n-1
-binary_M=double(binary_M.x);
+% binary_M=double(binary_M.x);
 
 
+
+nstop=nchoosek((k),error_weight);
+% binary_M_xor=gf(binary_M);
+e_vector_i_2=[zeros(1,(k)-error_weight),ones(1,error_weight)];
+jj=1;
+
+while jj<=nstop  % -1 for excluding the zero vector
+    e_vector_i_2=e_vector_i_2';
+    %     B2test_jj=gf(e_vector_i_2);
+    Test_solution=binary_M*e_vector_i_2;
+    solution=mod(Test_solution,(2^m)-1);
     
-         nstop=nchoosek((k),error_weight);
-         binary_M_xor=gf(binary_M);
-         e_vector_i_2=[zeros(1,(k)-error_weight),ones(1,error_weight)];
-         jj=1;
-         
-        while jj<=nstop  % -1 for excluding the zero vector
-            e_vector_i_2=e_vector_i_2';
-            B2test_jj=gf(e_vector_i_2);
-            Test_solution=binary_M_xor*B2test_jj;
-            solution=double(Test_solution.x)';
-            dist_vector=(solution~=query_codeword');
-            
-            if sum(dist_vector)<=error_weight
-                  
-               
-                % disp(['decoding t= ', num2str(error_weight),'/',num2str(error_max)])
-                 disp('YES, solution found')
-                
-                x=double(B2test_jj.x);
-                G=binary_M_xor;
-                y=solution';
-                %uncomment below for global code
-                % [G,y,y_query]=get_rows(binary_M_xor,dist_vector,solution,query_codeword);
-             
-                jj=nstop^2; 
-            else
-                G=[];
-                x=[];
-                y=[];
-                %                     disp(['decoding t=: ', num2str(error_weight),'/',num2str(error_max)])
-                
-                
-            end
-            jj=jj+1;
-            e_vector_i_2=nextperm(e_vector_i_2);
-        end
-
+    dist_vector=solution~=query_codeword;
     
-if isempty(G) && isempty(x) && isempty(y) && error_weight==error_max+1
+    if sum(dist_vector)<=error_weight
+        jj=nstop+1;
+        
+        % disp(['decoding t= ', num2str(error_weight),'/',num2str(error_max)])
+        %                 disp('YES, solution found')
+        
+        x=e_vector_i_2;
+        G=binary_M;
+        y=solution';
+        %uncomment below for global code
+        % [G,y,y_query]=get_rows(binary_M_xor,dist_vector,solution,query_codeword);
+        
+        
+    else
+        G=[];
+        x=[];
+        y=[];
+        %                     disp(['decoding t=: ', num2str(error_weight),'/',num2str(error_max)])
+        
+        
+    end
+    jj=jj+1;
+    e_vector_i_2=nextperm(e_vector_i_2);
+end
+
+
+if isempty(G) && isempty(x) && isempty(y) && error_weight==t+1
     %     disp('no solution')
 end
 end
 %%
-function [binary_M, query_codeword,proj_n_mat]=encode_T(x1,n,k,rx)
-[binary_M,proj_n_mat]=project_function(x1,n,k); % input ned row vector
-binary_M_gf=gf(binary_M);
-nx=gf(rx);
-query_codeword=(binary_M_gf*nx)';
-query_codeword=double(query_codeword.x)';
+function [ query_codeword,proj_n_mat]=encode_T(x1,n,k,m,rx)
+[binary_M,proj_n_mat]=project_function(x1,n,k,m); % input ned row vector
+
+
+query_codeword=binary_M*rx;
+query_codeword=mod(query_codeword,(2^m)-1);
 end
 %%
-function [binary_M,proj_n_mat]=project_function(input_Strig,n,k)
-proj_n_mat=randn(n*k,size(input_Strig,1));
+function [binary_M,proj_n_mat]=project_function(input_Strig,n,k,m)
+proj_n_mat=randn(n*k*m,size(input_Strig,1));
 strin_n_mat=proj_n_mat*input_Strig;
-binary_M=reshape(strin_n_mat,n,k);
-binary_M=sign(binary_M);
+binary_M=sign(strin_n_mat);
 binary_M(binary_M==-1)=0;
+binary_M=reshape(binary_M,n*k,m);
+binary_M=bi2de(binary_M);
+binary_M=reshape(binary_M,n,k);
 end
 %%
-function [binary_M]=project_function2(input_Strig,n,k,mat1)
+function [binary_M]=project_function2(input_Strig,n,k,m,mat1)
 strin_n_mat=mat1*input_Strig;
-binary_M=reshape(strin_n_mat,n,k);
-binary_M=sign(binary_M);
+binary_M=sign(strin_n_mat);
 binary_M(binary_M==-1)=0;
+binary_M=reshape(binary_M,n*k,m);
+binary_M=bi2de(binary_M);
+binary_M=reshape(binary_M,n,k);
 end
 %%
-function [G,x,y]=decoding(x2,n,k,error_max,query_codeword,proj_n_mat)
-[binary_M2]=project_function2(x2,n,k,proj_n_mat); % input ned row vector
-binary_M2=gf(binary_M2);
-[G,x,y]=Decode_T(binary_M2,k,error_max,query_codeword);
-end
-%%
-function [solut_mat,solut_v,query_v]=get_rows(binary_M_xor,dist_vector,solution,query_codeword)
-zero_idx=sort(find(dist_vector==0));
-solut_mat=binary_M_xor(zero_idx,:);
-solut_mat=double(solut_mat.x);
-solution=solution';
-query_codeword=query_codeword';
-solut_v=solution(zero_idx);
-query_v=query_codeword(zero_idx);
+function [G,x,y]=decoding(x2,n,k,t,m,query_codeword,proj_n_mat)
+[binary_M2]=project_function2(x2,n,k,m,proj_n_mat); % input ned row vector
+% binary_M2=gf(binary_M2);
+[G,x,y]=Decode_T(binary_M2,k,t,m,query_codeword);
 end
 %%
